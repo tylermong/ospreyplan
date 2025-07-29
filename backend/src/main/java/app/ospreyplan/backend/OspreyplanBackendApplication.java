2f9c2af326272e59c5e7a9b855208d9ef2f78cbc
tylermong <117101211+tylermong@users.noreply.github.com>
2025-07-29 10:28:07 -0400

    feat: init Spring Boot backend

---
+++ backend/src/main/java/app/ospreyplan/backend/OspreyplanBackendApplication.java
@@ -0,0 +1,13 @@
+package app.ospreyplan.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OspreyplanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OspreyplanBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/app/ospreyplan/backend/OspreyplanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ospreyplan.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OspreyplanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
