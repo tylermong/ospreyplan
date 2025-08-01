7566ffbd5653123681543de1646c95f00855dbbc
tylermong <117101211+tylermong@users.noreply.github.com>
2025-07-31 20:01:22 -0400

    fix: correct method name and annotation

--- backend/src/main/java/app/ospreyplan/backend/config/SecurityConfig.java
+++ backend/src/main/java/app/ospreyplan/backend/config/SecurityConfig.java
@@ -1,13 +1,15 @@
 package app.ospreyplan.backend.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfig
 {
     @Bean
-    public SecurityFilterChain filterChainer(HttpSecurity http) throws Exception
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
     {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**").permitAll().anyRequest().permitAll())
