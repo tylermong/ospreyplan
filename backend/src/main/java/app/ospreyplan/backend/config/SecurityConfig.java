5c54c4bbc77047bb43b5758fb08e11c7e29da589
tylermong <117101211+tylermong@users.noreply.github.com>
2025-07-31 20:03:40 -0400

    refactor: replace lambda with method reference

--- backend/src/main/java/app/ospreyplan/backend/config/SecurityConfig.java
+++ backend/src/main/java/app/ospreyplan/backend/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package app.ospreyplan.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,9 +12,9 @@ public class SecurityConfig
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
     {
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**").permitAll().anyRequest().permitAll())
-                .formLogin(form -> form.disable());
+                .formLogin(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
