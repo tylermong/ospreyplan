88d8cd081d05d2f8c05c35faa169333844aee372
tylermong <117101211+tylermong@users.noreply.github.com>
2025-08-14 21:20:48 -0400

    feat: create UserSettings service

---
+++ backend/src/main/java/app/ospreyplan/backend/usersettings/UserSettingsService.java
@@ -0,0 +1,25 @@
+package app.ospreyplan.backend.usersettings;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserSettingsService
+{
+    private final UserSettingsRepository repository;
+
+    public UserSettingsService(UserSettingsRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public UserSettings updateSettings(UUID userId, UserSettingsDTO dto)
+    {
+        UserSettings settings = repository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User ID not found"));
+
+        settings.setDegree(dto.getDegree());
+        settings.setStartYear(dto.getStartYear());
+
+        return repository.save(settings);
+    }
+}
