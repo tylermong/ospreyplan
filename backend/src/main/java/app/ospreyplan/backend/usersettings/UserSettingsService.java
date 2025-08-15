5102817f23c1ed51091e718519110f1e7bbcc27f
tylermong <117101211+tylermong@users.noreply.github.com>
2025-08-14 21:27:51 -0400

    refactor: reorder import statements

--- backend/src/main/java/app/ospreyplan/backend/usersettings/UserSettingsService.java
+++ backend/src/main/java/app/ospreyplan/backend/usersettings/UserSettingsService.java
@@ -1,8 +1,8 @@
 package app.ospreyplan.backend.usersettings;
 
-import org.springframework.stereotype.Service;
-
 import java.util.UUID;
+
+import org.springframework.stereotype.Service;
 
 @Service
 public class UserSettingsService
