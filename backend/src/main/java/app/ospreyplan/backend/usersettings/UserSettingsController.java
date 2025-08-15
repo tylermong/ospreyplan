f3825dcda3d9b2589b8deb03f29fff06db90211e
tylermong <117101211+tylermong@users.noreply.github.com>
2025-08-14 21:36:47 -0400

    refactor: remove unused imports

--- backend/src/main/java/app/ospreyplan/backend/usersettings/UserSettingsController.java
+++ backend/src/main/java/app/ospreyplan/backend/usersettings/UserSettingsController.java
@@ -1,13 +1,10 @@
 package app.ospreyplan.backend.usersettings;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/settings")
