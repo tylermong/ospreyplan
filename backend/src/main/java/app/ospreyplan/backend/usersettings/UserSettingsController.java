cf7ea7032342b06eefdd4e43428896ca81152e00
tylermong <117101211+tylermong@users.noreply.github.com>
2025-08-14 21:27:32 -0400

    refactor: update imports to not use wildcard

--- backend/src/main/java/app/ospreyplan/backend/usersettings/UserSettingsController.java
+++ backend/src/main/java/app/ospreyplan/backend/usersettings/UserSettingsController.java
@@ -1,7 +1,11 @@
 package app.ospreyplan.backend.usersettings;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.UUID;
 
