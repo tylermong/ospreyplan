c0b83d83f1f79a17bb7f29b48b55124686b99253
tylermong <117101211+tylermong@users.noreply.github.com>
2025-09-17 20:46:33 -0400

    refactor: remove unused method findByUserId

--- backend/src/main/java/app/ospreyplan/backend/planner/course/PlannedCourseRepository.java
+++ backend/src/main/java/app/ospreyplan/backend/planner/course/PlannedCourseRepository.java
@@ -2,10 +2,9 @@ package app.ospreyplan.backend.planner.course;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.UUID;
 
 public interface PlannedCourseRepository extends JpaRepository<PlannedCourse, UUID>
 {
-    List<PlannedCourse> findByUserId(UUID userId);
+    
 }
