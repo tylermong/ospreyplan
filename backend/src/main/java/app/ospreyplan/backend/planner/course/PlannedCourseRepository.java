7c7e7a02dddffbc7d2008c2e059931d46e16f21e
tylermong <117101211+tylermong@users.noreply.github.com>
2025-12-30 15:45:36 -0500

    feat: add method to retrieve planned courses by user ID ordered by creation date for audit

--- backend/src/main/java/app/ospreyplan/backend/planner/course/PlannedCourseRepository.java
+++ backend/src/main/java/app/ospreyplan/backend/planner/course/PlannedCourseRepository.java
@@ -2,9 +2,10 @@ package app.ospreyplan.backend.planner.course;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PlannedCourseRepository extends JpaRepository<PlannedCourse, UUID>
 {
-    
+    List<PlannedCourse> findByPlannedSemester_UserIdOrderByCreatedAtAsc(UUID userId);
 }
